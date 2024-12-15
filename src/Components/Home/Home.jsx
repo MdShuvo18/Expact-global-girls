d578b02cb3a97cb89a23c73a233b01a631d074bd
MdShuvo18 <mdks33881@gmail.com>
2024-12-16 01:23:08 +0600

    Work section added successfuly done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../Banner/Banner";
 import CommunitySection from "../CommunitySection/CommunitySection";
 import FAQsection from "../FAQsection/FAQsection";
 import TravelerSection from "../TravelerSection/TravelerSection";
+import WorkSection from "../WorksSection/WorkSection";
 
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <main>
                 <CommunitySection />
                 <TravelerSection/>
+                <WorkSection/>
                 <FAQsection/>
             </main>
         </div>
---
+++ src/Components/WorksSection/WorkSection.jsx
@@ -0,0 +1,56 @@
+import Headers from "../../reUseable/Headers";
+import img1 from "../../assets/image-resources/Filter-rafiki 1.svg"
+import img2 from "../../assets/image-resources/Friendship-pana 1.svg"
+import img3 from "../../assets/image-resources/Group 12.png"
+import img4 from "../../assets/image-resources/Vision board-bro 1.svg"
+import img5 from "../../assets/image-resources/Online friends-rafiki 1.svg"
+
+const WorkSection = () => {
+    return (
+        <div className="mt-24">
+            <Headers title="How It Works" subTitle="Search and find other travelers heading to the same destination." />
+            <div className="grid grid-cols-3 justify-items-center mt-16">
+                {/* left side */}
+                <div>
+                    <div>
+                        <h1 className="text-2xl font-semibold text-[#1D2939]">Plan Your Trip</h1>
+                        <p className="text-lg font-light text-[#475467]">Start by setting your travel dates, destinations, and preferences. This helps you tailor your search for housemates who align with your journey.</p>
+                        <div className="flex justify-center items-center">
+                            <img src={img1} alt="" />
+                        </div>
+                    </div>
+                    <div>
+                        <h1 className="text-2xl font-semibold text-[#1D2939]">Connect and Build Relationships</h1>
+                        <p className="text-lg font-light text-[#475467]">Send messages, ask questions, and get to know potential housemates before committing. Building rapport ensures a comfortable and trusted connection for your shared journey</p>
+                        <div className="flex justify-center items-center">
+                            <img src={img2} alt="" />
+                        </div>
+                    </div>
+                </div>
+                {/* middle */}
+                <div> <img src={img3} alt="" /></div>
+                {/* right side */}
+                <div>
+                    <div>
+                        <div className="flex justify-center items-center">
+                            <img src={img4} alt="" />
+                        </div>
+                        <h1 className="text-2xl font-semibold text-[#1D2939]">Explore Matching Profiles</h1>
+                        <p className="text-lg font-light text-[#475467]">Browse through profiles of like-minded women who are traveling to similar locations. You can filter results based on your travel dates, needs, and values.</p>
+
+                    </div>
+                    <div>
+                        <div className="flex justify-center items-center">
+                            <img src={img5} alt="" />
+                        </div>
+                        <h1 className="text-2xl font-semibold text-[#1D2939]">Secure Your Plans & Enjoy the Journey</h1>
+                        <p className="text-lg font-light text-[#475467]">Once you’ve found the right housemate, finalize your arrangements with confidence. With a trusted companion, you can travel with peace of mind and enjoy every moment.</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WorkSection;
