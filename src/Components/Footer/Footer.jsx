0d1d29c25d0b4f6fe25c0fd8f4302b903a3ef5b5
MdShuvo18 <mdks33881@gmail.com>
2024-12-16 13:38:08 +0600

    footer updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,10 +7,10 @@ import facebook from "../../assets/image-resources/facebook 1.svg"
 const Footer = () => {
     return (
         <footer className="mt-24 bg-white">
-            <div className="flex  mx-32 gap-16">
-                <div className="space-y-7">
+            <div className="flex justify-between mx-36 gap-16">
+                <div className="flex w-1/3 flex-col">
                     <img className="w-[130px] h-[105px]" src={img} alt="" />
-                    <h1 className="w-2/3">Scan a Turning Hearts Medallion to learn about and interact with those who have passed on. You can also begin documenting the legacy you wish to leave behind.</h1>
+                    <h1 className="">Scan a Turning Hearts Medallion to learn about and interact with those who have passed on. You can also begin documenting the legacy you wish to leave behind.</h1>
                     <div className="space-y-4">
                         <p>Social Media :</p>
                         <div className="flex gap-3">
@@ -22,21 +22,22 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="flex items-center gap-36">
-                    <div className="">
-                        <h1 className="text-lg font-bold text-[#1D2939]">Quick Link</h1>
-                        <ul>
-                            <li>Home</li>
-                            <li>FAQ</li>
-                            <li>Country search</li>
-                            <li>Register</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h1>(316) 555-0116</h1>
-                        <p>deanna.curtis@example.com</p>
-                    </div>
+                <div className="">
+                    <h1 className="text-lg font-bold text-[#1D2939]">Quick Link</h1>
+                    <ul>
+                        <li>Home</li>
+                        <li>FAQ</li>
+                        <li>Country search</li>
+                        <li>Register</li>
+                    </ul>
+                </div>
+                <div>
+                    <h1>(316) 555-0116</h1>
+                    <p>deanna.curtis@example.com</p>
                 </div>
             </div>
+            </div>
+
             <div className="mt-10 border-t border-gray-700 pt-6 text-end">
                 <p>&copy; {new Date().getFullYear()} Ash and Fire. All Rights Reserved.</p>
             </div>
